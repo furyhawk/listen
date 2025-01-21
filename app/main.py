09639aef8fe18b94463534778dffe174e19913c7
Teck Meng <furyx@hotmail.com>
2025-01-21 09:24:58 +0800

    bump version to 6.1.0

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app.core.config import get_settings
 
 app = FastAPI(
     title="minimal fastapi postgres",
-    version="6.0.0",
+    version="6.1.0",
     description="https://github.com/furyhawk/minimal-fastapi-postgres-template",
     openapi_url="/openapi.json",
     docs_url="/",
